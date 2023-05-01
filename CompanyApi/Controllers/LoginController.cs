503728abacb266cbe9123d5e4e8419facad436a3
adef <basc11111@gmail.com>
2023-05-01 16:34:18 +0300

    auth

---
+++ CompanyApi/Controllers/LoginController.cs
@@ -0,0 +1,64 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using CompanyApi.Context;
+using CompanyApi.Models;
+using CompanyApi.Models.Posgres;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+
+namespace CompanyApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class LoginController : ControllerBase
+{
+    private readonly HardDriveCompanyContext _context;
+
+    public LoginController(HardDriveCompanyContext context)
+    {
+        _context = context;
+    }
+
+    [AllowAnonymous]
+    [HttpPost]
+    public async Task<ActionResult> Login([FromBody] EmployeeLogin employeeLogin)
+    {
+        Employee? employeeP;
+
+        await using (HardDriveCompanyContext db = new HardDriveCompanyContext())
+        {
+            employeeP = db.Employees
+                .FromSqlRaw("SELECT * FROM employee WHERE employee_login = {0} AND password = crypt({1}, password)",
+                    employeeLogin.Login,
+                    employeeLogin.Password)
+                .FirstOrDefault();
+        }
+
+        if (employeeP == null)
+        {
+            return NotFound("Не верные данные");
+        }
+        
+        var securityKey = AuthOptions.GetSymmetricSecurityKey();
+        var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+        var claims = new[]{
+            new Claim(ClaimTypes.NameIdentifier, employeeP.EmployeeLogin)
+        };
+                
+        var token = new JwtSecurityToken(
+            AuthOptions.ISSUER,
+            AuthOptions.AUDIENCE,
+            claims,
+            expires: DateTime.Now.AddHours(12),
+            signingCredentials: credentials);
+
+        return Ok(new
+        {
+            token = new JwtSecurityTokenHandler().WriteToken(token),
+            expiration = token.ValidTo,
+        });
+    }
+}
--- CompanyApi/Program.cs
+++ CompanyApi/Program.cs
@@ -1,6 +1,7 @@
 using CompanyApi;
 using CompanyApi.Context;
 using CompanyApi.Services;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -15,7 +16,35 @@ builder.Services.AddSingleton<EmployeeService>();
 
 builder.Services.AddDbContext<HardDriveCompanyContext>();
 
+builder.Services.AddSingleton(AuthOptions.GetSymmetricSecurityKey());
+
 builder.Services.AddControllers();
+
+const string jwtSchemeName = "JwtBearer";
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultAuthenticateScheme = jwtSchemeName;
+        options.DefaultChallengeScheme = jwtSchemeName;
+    })
+    .AddJwtBearer(jwtSchemeName,jwtBearerOptions => 
+    {
+        jwtBearerOptions.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidIssuer = AuthOptions.ISSUER,
+            ValidateAudience = true,
+            ValidAudience = AuthOptions.AUDIENCE,
+            ValidateLifetime = true,
+            IssuerSigningKey = AuthOptions.GetSymmetricSecurityKey(),
+            ValidateIssuerSigningKey = true,
+		
+            ClockSkew = TimeSpan.FromSeconds(30)
+        };
+    });
+
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -31,8 +60,13 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseRouting();
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 app.Run();
